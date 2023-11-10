b19a36fb3dc926f8945e68899ba46d8799fbd545
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-11-10 00:22:09 -0600

    working through navbar styling and creation

---
+++ src/main/resources/static/js/partials.js
@@ -0,0 +1,12 @@
+function navbarIconToggle() {
+    let nav = document.querySelector(".nav-icon");
+    nav.addEventListener("onclick", function() {
+       if(nav.style.display === "block") {
+           nav.style.display = "none";
+       } else {
+           nav.style.display = "block";
+       }
+    });
+}
+
+navbarIconToggle();
